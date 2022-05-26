465d2869adad17bbdb21f008807e47d706418854
RPL-32 <rizkiromadhanbks298@gmail.com>
2022-05-26 08:25:23 +0700

    Jasa Online

--- app/src/main/java/rizkiromadhan/polbeng/ac/jasaonline/JasaFragment.kt
+++ app/src/main/java/rizkiromadhan/polbeng/ac/jasaonline/JasaFragment.kt
@@ -28,7 +28,6 @@ class JasaFragment : Fragment() {
             param2 = it.getString(ARG_PARAM2)
         }
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
